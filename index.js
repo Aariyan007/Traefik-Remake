944ce1e79fe832df6f77e80217a2dccc9ac679e5
Aariyan007 <aariyansunu28@gmail.com>
2026-01-29 01:45:04 +0530

    round robin added

--- index.js
+++ index.js
@@ -17,29 +17,28 @@ docker.getEvents(function (err, stream) {
         console.log("Error in getting docker events:", err);
         return;
     }
-    
+
     stream.on('data', async (chunk) => {
         if (!chunk) return;
-        
+
         const event = JSON.parse(chunk.toString());
-        
+
         if (event.Type === "container" && event.Action === "start") {
             const container = docker.getContainer(event.Actor.ID);
             const containerInfo = await container.inspect();
             const containerName = containerInfo.Name.substring(1);
-            
+
             const networks = containerInfo.NetworkSettings.Networks;
             console.log('Container ' + containerName + ' networks:', Object.keys(networks));
-            
+
             let ipAddress = null;
             let networkName = null;
-            
+
             const possibleNetworkNames = [
                 'proxy-net',
                 'reverse-proxy_proxy-net',
                 'trafeik_proxy-net'
             ];
-            
             for (const name of possibleNetworkNames) {
                 if (networks[name] && networks[name].IPAddress) {
                     ipAddress = networks[name].IPAddress;
@@ -47,7 +46,6 @@ docker.getEvents(function (err, stream) {
                     break;
                 }
             }
-            
             if (!ipAddress) {
                 for (const [name, network] of Object.entries(networks)) {
                     if (network.IPAddress) {
@@ -57,29 +55,57 @@ docker.getEvents(function (err, stream) {
                     }
                 }
             }
-            
+
             if (!ipAddress) {
                 console.log('Warning: No IP address found for container ' + containerName);
                 console.log('Available networks:', JSON.stringify(networks, null, 2));
                 return;
             }
-            
+
             const exposedPorts = containerInfo.Config.ExposedPorts;
             let defaultPort = null;
-            
+
             if (exposedPorts) {
                 const portKey = Object.keys(exposedPorts)[0];
                 if (portKey) {
                     defaultPort = portKey.split("/")[0];
                 }
             }
-            
+
             console.log('Registering ' + containerName + '.localhost --> http://' + ipAddress + ':' + defaultPort + ' (network: ' + networkName + ')');
-            
-            db.set(containerName, {
+
+            const labels = containerInfo.Config.Labels || {};
+            const serviceName = labels.service || containerName;
+
+            if (!db.has(serviceName)) {
+                db.set(serviceName, { targets: [], index: 0 });
+            }
+
+            db.get(serviceName).targets.push({
                 ipAddress,
-                defaultPort
+                port: defaultPort,
+                containerId: event.Actor.ID
             });
+
+            console.log('Service "' + serviceName + '" now has ' + db.get(serviceName).targets.length + ' target(s)');
+        }
+
+        if (event.Type === "container" && event.Action === "die") {
+            const containerId = event.Actor.ID;
+
+            for (const [serviceName, service] of db.entries()) {
+                const initialLength = service.targets.length;
+                service.targets = service.targets.filter(t => t.containerId !== containerId);
+
+                if (service.targets.length < initialLength) {
+                    console.log('Removed container from service "' + serviceName + '", ' + service.targets.length + ' target(s) remaining');
+                }
+
+                if (service.targets.length === 0) {
+                    db.delete(serviceName);
+                    console.log('Service "' + serviceName + '" removed (no targets)');
+                }
+            }
         }
     });
 });
@@ -89,22 +115,23 @@ const reverseProxyApp = express();
 reverseProxyApp.use(function (req, res) {
     const hostName = req.hostname;
     const subDomain = hostName.split(".")[0];
-    
+
     if (!db.has(subDomain)) {
-        console.log('Container not found for subdomain: ' + subDomain);
-        return res.status(404).send("Container Not Found");
+        console.log('Service not found for subdomain: ' + subDomain);
+        return res.status(404).send("Service Not Found");
     }
-    
-    const { ipAddress, defaultPort } = db.get(subDomain);
-    
-    if (!defaultPort) {
-        console.log('No exposed port for container: ' + subDomain);
-        return res.status(502).send("No exposed port");
+    const service = db.get(subDomain);
+    const { targets } = service;
+    if (!targets.length) {
+        console.log('No available targets for service: ' + subDomain);
+        return res.status(502).send("No available targets");
     }
-    
-    const target = 'http://' + ipAddress + ':' + defaultPort;
-    console.log('Proxying request for ' + hostName + ' to ' + target);
-    
+
+    const targetInfo = targets[service.index];
+    service.index = (service.index + 1) % targets.length;
+    const target = 'http://' + targetInfo.ipAddress + ':' + targetInfo.port;
+    console.log('Proxying request for ' + hostName + ' to ' + target + ' [' + (service.index === 0 ? targets.length : service.index) + '/' + targets.length + ']');
+
     return proxy.web(req, res, { target, changeOrigin: true, ws: true });
 });
 
@@ -115,15 +142,15 @@ managementAPI.use(express.json());
 
 managementAPI.post('/containers', async (req, res) => {
     try {
-        const { image, tag = "latest" } = req.body;
-        
+        const { image, tag = "latest", replicas = 1, service } = req.body;
+
         if (!image) {
             return res.status(400).json({ error: "Image name is required" });
         }
-        
+
         const images = await docker.listImages();
         let imageExists = false;
-        
+
         for (const systemImg of images) {
             if (!systemImg.RepoTags) continue;
             for (const systemTag of systemImg.RepoTags) {
@@ -134,7 +161,7 @@ managementAPI.post('/containers', async (req, res) => {
             }
             if (imageExists) break;
         }
-        
+
         if (!imageExists) {
             console.log('Pulling image: ' + image + ':' + tag);
             const stream = await docker.pull(image + ':' + tag);
@@ -145,60 +172,77 @@ managementAPI.post('/containers', async (req, res) => {
                 });
             });
         }
-        
+
         const networksList = await docker.listNetworks();
         let proxyNetworkName = null;
-        
+
         for (const network of networksList) {
             if (network.Name.includes('proxy-net')) {
                 proxyNetworkName = network.Name;
                 break;
             }
         }
-        
+
         if (!proxyNetworkName) {
             console.error('proxy-net network not found!');
             return res.status(500).json({ error: "Proxy network not found" });
         }
-        
+
         console.log('Using network: ' + proxyNetworkName);
-        
-        const container = await docker.createContainer({
-            Image: image + ':' + tag,
-            Tty: false,
-            HostConfig: {
-                AutoRemove: true,
-                NetworkMode: proxyNetworkName
+
+        const createdContainers = [];
+        for (let i = 0; i < replicas; i++) {
+            const containerConfig = {
+                Image: image + ':' + tag,
+                Tty: false,
+                HostConfig: {
+                    AutoRemove: true,
+                    NetworkMode: proxyNetworkName
+                }
+            };
+            if (service) {
+                containerConfig.Labels = { service: service };
             }
-        });
-        
-        await container.start();
-        
-        const containerInfo = await container.inspect();
-        const containerName = containerInfo.Name.substring(1);
-        
+
+            const container = await docker.createContainer(containerConfig);
+            await container.start();
+
+            const containerInfo = await container.inspect();
+            const containerName = containerInfo.Name.substring(1);
+            createdContainers.push(containerName);
+        }
+
+        const serviceName = service || createdContainers[0];
+
         return res.json({
             status: "success",
-            container: containerName + '.localhost',
-            url: 'http://' + containerName + '.localhost'
+            service: serviceName,
+            url: 'http://' + serviceName + '.localhost',
+            containers: createdContainers,
+            replicas: replicas
         });
-        
+
     } catch (error) {
         console.error('Error creating container:', error);
-        return res.status(500).json({ 
-            error: error.message 
+        return res.status(500).json({
+            error: error.message
         });
     }
 });
 
-managementAPI.get('/containers', (req, res) => {
-    const containers = Array.from(db.entries()).map(([name, info]) => ({
+managementAPI.get('/services', (req, res) => {
+    const services = Array.from(db.entries()).map(([name, service]) => ({
         name,
         url: 'http://' + name + '.localhost',
-        target: 'http://' + info.ipAddress + ':' + info.defaultPort
+        targets: service.targets.map(t => ({
+            ip: t.ipAddress,
+            port: t.port,
+            containerId: t.containerId.substring(0, 12)
+        })),
+        activeTargets: service.targets.length
     }));
-    
-    res.json({ containers });
+
+    res.json({ services });
 });
 
 managementAPI.listen(8080, () => {
